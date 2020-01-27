59f5e111749c526ccdd89065d6cba68fe8307460
yashvardhanpal26 <59726927+yashvardhanpal26@users.noreply.github.com>
2020-01-27 22:43:03 +0530

    Assignment Submitted

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -3,40 +3,85 @@ using namespace std;
 
 class Polynomial
 {
-  private:
-    // Variables to store information about polynomial
-  public:
-    Polynomial()
-    {
-      // Behavior of default constructor
-    }
-
-    Polynomial(int deg)
-    {
-      // Behavior of constructor with arguments
-    }
-  
-    ~Polynomial()
-    {
-      // Behavior of destructor
-    }
-  
-    //  Overload copy constructor, +, - and = operators
-  
-    void storePolynomial()
-    {
-      //  Code to enter and store polynomial
-    }
-    void display()
-    {
-      //  Code to print the polynomial in readable format
-    }
-  
-}
-
+	int array[100];
+	int power;
+	
+	 // Variables to store information about polynomial
+	 public:
+	 	
+	 	Polynomial()
+	 	{
+	 		power=1;
+		}
+		Polynomial(int deg)
+		{
+			// Behavior of constructor with arguments
+			cout<<"enter the degree of "<<deg<<" polynomial ";
+			cin>>power;
+			power++;	
+		}
+		~Polynomial()
+        {
+             // Behavior of destructor
+        }
+         //  Overload copy constructor, +, - and = operators
+        Polynomial operator +(Polynomial d)
+		{
+			Polynomial p;
+			p.power=this->power>=d.power? this->power:d.power;
+			for(int i=0;i<p.power;i++)
+			{
+				if(i>=this->power)
+				  this->array[i]=0;
+				if(i>=d.power)
+				  d.array[i]=0;
+				  p.array[i]=d.array[i]+this->array[i];  
+			}
+			return p;
+		}
+		Polynomial operator -(Polynomial d)
+		{
+			Polynomial p;
+			p.power=this->power>=d.power? this->power:d.power;
+			for(int i=0;i<p.power;i++)
+			{
+				if(i>=this->power)
+				  this-> array[i]=0;
+				if (i>=d.power)
+				  d.array[i]=0;
+				  p.array[i]=this->array[i]-d.array[i];  
+			} 
+		    return p;	
+	    }
+	    Polynomial operator =(Polynomial d)
+		{
+			Polynomial p;
+			this->power=d.power;
+			for(int i=0;i<this->power;i++)
+			  this->array[i]=d.array[i];
+	    }
+	    void storePolynomial()
+        {
+                //  Code to enter and store polynomial	
+            cout<<"\n enter the cofficient with respect to its ascending order of power"<<endl;
+			for(int i=0;i<power;i++)
+			cin>>array[i];
+ 	    }
+ 	    void display()
+        {
+            //  Code to print the polynomial in readable format
+            cout<<"\n";
+            for(int i=power-1;i>=0;i--)
+            {
+            	cout<<array[i]<<"X^"<<i;
+            	if(i>0)
+            	  cout<<" + ";
+			}
+	    }
+};
 int main()
 {
-  int degFirst, degSecond;
+	  int degFirst=1, degSecond=2;
   // Ask user to input the values of degFirst and degSecond 
   Polynomial firstPolynomial(degFirst);
   Polynomial secondPolynomial(degSecond);
@@ -48,8 +93,16 @@ int main()
   thirdPolynomial=firstPolynomial+secondPolynomial;
   Polynomial fourthPolynomial=firstPolynomial-secondPolynomial;
   
+  cout<<"\n the first polynomial is :";
   firstPolynomial.display();
+  cout<<endl;
+  cout<<"\n the second polynomial is : ";
   secondPolynomial.display();
+  cout<<endl;
+  cout<<"\n the third polynomial is :";
   thirdPolynomial.display();
+  cout<<endl;
+  cout<<"\n the fourth polynomial is :";
   fourthPolynomial.display();
+
 }
